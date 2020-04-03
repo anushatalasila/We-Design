c85743117477893fcc1f74750132d7c06da0afd8
Parul Vig <vig.pa@husky.neu.edu>
2020-04-02 21:28:19 -0400

    Changes to rest api schema, controller on server side for User

---
+++ Server/routes/index.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const weModel = require('./user');
---
+++ Server/services/service.js
@@ -0,0 +1,54 @@
+'use strict';
+const mongoose = require('mongoose'),
+    User = mongoose.model('user');
+
+/**
+ * Returns a promise for search results.
+ *
+ * @param search param.
+*/
+exports.search = (params) => {
+    const promise = User.find(params).exec();
+    return promise;
+};
+
+/**
+ * Saves the new user object.
+ *
+ * @param user
+*/
+exports.save = (user) => {
+    const newuser = new User(user);
+    return newuser.save();
+};
+
+/**
+ * Returns the user object by id.
+ *
+ * @param userId
+*/
+exports.get = (userId) => {
+    const userPromise = Todo.findById(userId).exec();
+    return userPromise;
+};
+
+/**
+ * Updates an existing user.
+ *
+ * @param updateduser
+*/
+exports.update = (updateduser) => {
+    
+const promise = User.findByIdAndUpdate(updateduser.id, updateduser).exec();
+   return promise;
+};
+
+/**
+ * Deletes an existing user item.
+ *
+ * @param UserId
+*/
+exports.delete = (UserId) => {
+    const promise = User.findByIdAndRemove(UserId).exec();
+    return promise;
+};
