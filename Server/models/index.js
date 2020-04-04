4edc6a1838653d280d652d7afcb5f50d83dff6cd
Parul Vig <vig.pa@husky.neu.edu>
2020-04-04 14:04:13 -0400

    User Rest api changes

---
+++ Server/models/index.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const userModel = require('./user');
--- Server/models/user.js
+++ Server/models/user.js
@@ -23,9 +23,9 @@ let user = new Schema({
     userEmailID: {
         type: String,
         unique: true,
-        required: 'Email address is required',
-        validate: [validateEmail, 'Please fill a valid email address'],
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        required: "Email address is required"
+        //,validate: [validateEmail, 'Please fill a valid email address'],
+        //match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
 });
 
