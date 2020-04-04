33155b1c7c6bad73b4b1841097650207b043ef23
anushatalasila <talasila.a@husky.neu.edu>
2020-04-04 17:41:11 -0400

    house APIs

--- Server/models/house.js
+++ Server/models/house.js
@@ -36,31 +36,4 @@ let house = new Schema({
     },
     
 });
-
-// schema of user details
-// let user = new Schema({
-//     userID: {
-//         type: String,
-//         required: "user ID is required"
-//     },
-    
-//     userFirstName: {
-//         type: String,
-//         required: "User First name is required"
-//     },
-
-//     userLastName: {
-//         type: String,
-//         required: "User Last Name is required"
-//     },
-
-//     userEmailID: {
-//         type: String,
-//         unique: true,
-//         required: 'Email address is required',
-//         validate: [validateEmail, 'Please fill a valid email address'],
-//         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-//     },
-// });
 module.exports = mongoose.model('house', house);
-// module.exports = mongoose.model('user', user);
