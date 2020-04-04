a48101002e2409b6cb00bb5ce6637ea3e2861693
Parul Vig <vig.pa@husky.neu.edu>
2020-04-04 14:41:54 -0400

    User Rest api changes/Password bcrypt

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,7 +1,8 @@
 'use strict';
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const bcrypt = require('bcrypt'),
+SALT_WORK_FACTOR = 10;
 var validateEmail = function(userEmailID) {
     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return re.test(userEmailID)
@@ -41,5 +42,32 @@ let user = new Schema({
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
 });
+user.pre('save', function(next) {
+    var curuser = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!curuser.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(curuser.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            curuser.password = hash;
+            next();
+        });
+    });
+});
+
+user.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model('user', user);
